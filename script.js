4d6468ab7fee8637a0cbc501a8caddbe06f67915
sharrmeen <sharmeenk2020@gmail.com>
2024-09-16 03:40:59 +0530

    new file:   fonts/digital-7.ttf 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -15,6 +15,38 @@ function divide(a,b){
         return "Nice Try"
     return a/b
 }
+let a;
+let b;
+let operator=null;
 
 const answ = add(10,5)
 console.log("Answer"+answ)
+
+const btns = document.querySelectorAll('.button')
+
+const generateExp=(event)=>{
+    const button=event.target
+    if(button.classList.contains('num') && operator===null)
+        {a+=button.textContent
+        // console.log("Number is clicked "+button.textContent)
+        console.log(a)
+        }
+
+    else if(button.classList.contains('num') && operator!==null)
+    {
+        b+=button.textContent
+        console.log(b)
+    }
+
+    else if(button.classList.contains('op') && operator===null)
+       { operator=button.textContent
+        console.log(operator)
+        }
+
+        
+
+}
+btns.forEach(btn=>{
+    btn.addEventListener("click",generateExp)
+})
+
