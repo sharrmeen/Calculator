352755ba65aafa45a7c18f0729679cc2c209bd57
sharrmeen <sharmeenk2020@gmail.com>
2024-09-17 02:20:02 +0530

    completed js logic

--- script.js
+++ script.js
@@ -15,38 +15,113 @@ function divide(a,b){
         return "Nice Try"
     return a/b
 }
-let a;
-let b;
+let a='';
+let b='';
 let operator=null;
 
-const answ = add(10,5)
-console.log("Answer"+answ)
-
+const display=document.querySelector('.display')
 const btns = document.querySelectorAll('.button')
 
 const generateExp=(event)=>{
     const button=event.target
     if(button.classList.contains('num') && operator===null)
         {a+=button.textContent
-        // console.log("Number is clicked "+button.textContent)
-        console.log(a)
+        
+        display.textContent=a
+        
         }
 
     else if(button.classList.contains('num') && operator!==null)
     {
         b+=button.textContent
-        console.log(b)
+        display.textContent=b
+    
     }
 
     else if(button.classList.contains('op') && operator===null)
        { operator=button.textContent
-        console.log(operator)
-        }
-
         
+        display.textContent=operator
+        }
+    else if(button.classList.contains('equal') && (a!=='' && b!=='' && operator!==null) )
+    {
+       let answer = operate(a,b,operator)
+       if(typeof answer !=='number')
+        display.textContent=answer
+     
+       else{
+       answer = parseFloat(answer.toFixed(2))
+       a=answer
+       operator=null
+       b=''
+       
+       display.textContent=answer}
+    }
+
+    else if(button.classList.contains('clear')){
+        a=''
+        b=''
+        operator=null
+        display.textContent=''
+    }
+
+    else if(button.classList.contains('del')){
+        if(operator===null)
+        {
+            a=a.slice(0,-1) 
+            display.textContent=a
+        }
+        else
+        {
+            b=b.slice(0,-1)
+            display.textContent=b
+        }
+    }
+
+    else if(button.classList.contains('decimal')){
+        if(operator===null && !a.includes('.'))
+            {
+                a+='.' 
+                display.textContent=a
+            }
+
+            else if(!b.includes('.'))
+            {
+                b+='.'
+                display.textContent=b
+            }
+    }
+
 
 }
+
+
 btns.forEach(btn=>{
     btn.addEventListener("click",generateExp)
 })
 
+function operate(a,b,operator){
+    a=parseFloat(a)
+    b=parseFloat(b)
+    switch (operator) {
+        case '+':
+            return  add(a,b)
+            
+    
+        case '-':
+            return sub(a,b)
+            
+    
+        case '*':
+            return multiply(a,b)
+            
+    
+        case '/':
+            return divide(a,b)            
+            
+    
+        default:
+            return ("enter valid expression")
+            
+    }
+}
